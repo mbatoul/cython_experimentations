40c08a7a6b93c7128c9f5b870e8025c0be68787e
mbatoul <batoulmathis@gmail.com>
2021-05-26 21:10:50 +0200

    Benchmark sequential computation before parallel computation

--- src/pairwise_dist/_pairwise_dist.pyx
+++ src/pairwise_dist/_pairwise_dist.pyx
@@ -34,18 +34,24 @@ cdef void _pairwise_dist(
     floating[:, ::1] X_a, # IN
     floating[:, ::1] X_b, # IN
     floating[:, ::1] distances, # OUT
+    bint parallel=0
 ) nogil:
     cdef:
         int i, j
         int n_rows_X_a = X_a.shape[0]
         int n_rows_X_b = X_b.shape[0]
         int n_features = X_a.shape[1]
-        
-    for i in prange(n_rows_X_a, nogil=True):
-        for j in range(n_rows_X_b):
-            distances[i, j] = _l1_distance(X_a, i, X_b, j, n_features)
+    
+    if parallel:
+        for i in prange(n_rows_X_a, nogil=True):
+            for j in range(n_rows_X_b):
+                distances[i, j] = _l1_distance(X_a, i, X_b, j, n_features)
+    else:
+        for i in range(n_rows_X_a):
+            for j in range(n_rows_X_b):
+                distances[i, j] = _l1_distance(X_a, i, X_b, j, n_features)
 
-def pairwise_dist(
+def pairwise_dist_sequential(
     floating[:, ::1] X_a,
     floating[:, ::1] X_b
 ):
@@ -53,6 +59,18 @@ def pairwise_dist(
     cdef:
         floating[:, ::1] distances = np.zeros([X_a.shape[0], X_b.shape[0]], dtype=float_dtype)
     
-    _pairwise_dist(X_a, X_b, distances)
+    _pairwise_dist(X_a, X_b, distances, parallel=0)
+    
+    return np.asarray(distances)
+
+def pairwise_dist_parallel(
+    floating[:, ::1] X_a,
+    floating[:, ::1] X_b
+):
+    float_dtype = np.float32 if floating is float else np.float64
+    cdef:
+        floating[:, ::1] distances = np.zeros([X_a.shape[0], X_b.shape[0]], dtype=float_dtype)
+    
+    _pairwise_dist(X_a, X_b, distances, parallel=1)
     
     return np.asarray(distances)
